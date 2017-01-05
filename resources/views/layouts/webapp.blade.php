8dee3e59c051d630cd10ad620b38cb6e2e0bb47a
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-05 02:17:23 -0200

    Creating view templates

--- resources/views/layouts/webapp.blade.php
+++ resources/views/layouts/webapp.blade.php
@@ -66,33 +66,55 @@
                         </button>
 
                         <!-- Branding Image/Name -->
-                        <a class="navbar-brand" href="{{ url('/') }}">
-                            {{ config('app.name', 'XNotes') }}
-                        </a>
+                        @if(Auth::guest())
+                            <a class="navbar-brand" href="{{ url('/') }}">
+                                <b>{{ '< ' . config('app.name', 'XNotes') . ' />' }}</b>
+                            </a>
+                        @else
+                            <a class="navbar-brand" href="{{ url('/notes') }}">
+                               <b>{{ '< ' . config('app.name', 'XNotes') . ' />' }}</b>
+                            </a>
+                        @endif
+                        
                     </div>
 
                     <div class="collapse navbar-collapse" id="app-navbar-collapse">
                         
                         <!-- Left side of navbar -->
-                        <ul class="nav navbar-nav">&nbsp;</ul>
+                        <ul class="nav navbar-nav">
+                            @if(Auth::guest())
+                                &nbsp;
+                            @else
+                                <button type="button" class="btn btn-primary navbar-btn">Nova Nota</button>
+                                <button type="button" class="btn btn-success navbar-btn">Nova Lista</button>
+                                <button type="button" class="btn btn-default navbar-btn">Nova Categoria</button>
+                            @endif
+                        </ul>
 
                         <!-- Right side of navbar -->
                         <ul class="nav navbar-nav navbar-right">
-                        
-                            <!-- Authentication Links -->
+                            <!-- Authentication Link -->
                             @if(Auth::guest())
                                 <li><a href="{{ url('/login') }}">Login</a></li>
                                 <li><a href="{{ url('/register') }}">Register</a></li>
                             @else
+                                <form class="navbar-form navbar-left">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Procurar por alguma nota..." size="50"/>
+                                    </div>
+                                </form>
                                 <li class="dropdown">
                                     <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                         {{ Auth::user()->name }} <span class="caret"></span>
                                     </a>
                                     <ul class="dropdown-menu" role="menu">
+                                        <li><a href="">Meu Perfil</a></li>
+                                        <li><a href="">Configurações</a></li>
+                                        <li role="separator" class="divider"></li>
                                         <li>
                                             <a href="{{ url('/logout') }}"
                                                 onclick="event.preventDefault();document.getElementById('logout-form').submit();">
-                                                Logout
+                                                Sair
                                             </a>
                                             <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                                                 {{ csrf_field() }}
--- resources/views/webapp/home.blade.php
+++ resources/views/webapp/home.blade.php
@@ -1,19 +1,37 @@
 @extends('layouts.webapp')
 
 @section('content')
-
+    @if(true)
     <div class="container">
         <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Dashboard</div>
+            <div class="col-md-8">
+                
+               <p>Notas Aqui!</p>
 
+            </div>
+            <div class="col-md-4">
+                
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">Categorias</h3>
+                    </div>
                     <div class="panel-body">
-                        You are logged in!
+                        
+                        <ul class="nav nav-pills nav-stacked">
+                            <li role="presentation" class="active"><a href="#">Todos <span class="badge">16</span></a></li>
+                            <li role="presentation"><a href="#">Social <span class="badge">6</span></a></li>
+                            <li role="presentation"><a href="#">Útil <span class="badge">8</span></a></li>
+                            <li role="presentation"><a href="#">Fitness <span class="badge">2</span></a></li>
+                        </ul>
+
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
+    @else
+        <h1 class="empty">Sem Notas</h1>
+    @endif
 
 @endsection
