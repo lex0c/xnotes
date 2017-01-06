19260f94050dc87b975ed94b7d47187ef8149e21
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-06 02:06:37 -0200

    Implementing note..

--- resources/views/webapp/home.blade.php
+++ resources/views/webapp/home.blade.php
@@ -5,10 +5,22 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8">
-                
-               <div class="panel panel-primary">
+
+            @foreach($notes as $note)
+
+                @php
+                    unset($createdAt);
+                    unset($updatedAt);
+                    {{-- Concatenates an 'as' between date and time --}}             
+                    $createdAt = explode(' ', $note->created_at);
+                    $createdAt = $createdAt[0] . ' as ' . $createdAt[1];
+                    $updatedAt = explode(' ', $note->updated_at);
+                    $updatedAt = $updatedAt[0] . ' as ' . $updatedAt[1];
+                @endphp
+
+               <div class="panel panel-{{ 'primary' }}">
                    <div class="panel-heading clearfix">
-                       <p class="left">Criado em 2016-12-27 as 22:58</p>
+                       <p class="left">Criado em {{ $createdAt }}</p>
                        <div class="btn-group right">
                            <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> Editar</button>
                            <button type="button" class="btn btn-danger dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -23,24 +35,23 @@
                     <div class="panel-body">                      
                         <div class="media">
                             <div class="media-left">
-                                <a href="#">
-                                    <img class="media-object" src="http://lorempixel.com/64/64/abstract/" alt="lorempixel">
-                                </a>
+                                <img title="Lorempixel Image" class="media-object" src="http://lorempixel.com/64/64/abstract/" alt="Imagens aleatórias sem significado."/>
                             </div>
                             <div class="media-body">
-                                <h4 class="media-heading">Media heading</h4>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vitae lorem at nibh egestas sagittis et et nulla. Integer ac tempus felis.
+                                <h4 class="media-heading"><b>{{ $note->title }}</b></h4>
+                                {{ $note->content }}
                             </div>
                         </div>
                     </div>
                     <div class="panel-footer clearfix">
-                        <a href="#" class="nolink"><span class="label label-info">Útil</span></a>
-                        <a href="#" class="nolink"><span class="label label-info">Hoje</span></a>
-                        <a href="#" class="nolink"><span class="label label-info">Faculdade</span></a>
-                        <a href="#" class="nolink"><span class="label label-info">Comprar</span></a>
-                        <p class="right litle-text">Editado 2016-12-27 as 22:58</p>
+                        
+                        @foreach($categories as $category)
+                            <a href="#" class="nolink"><span class="label label-info">{{ $category->name }}</span></a>
+                        @endforeach
+                        <p class="right litle-text">Editado {{ $updatedAt }}</p>
                     </div>
                 </div>
+            @endforeach
 
             </div>
             <div class="col-md-4">
@@ -50,21 +61,15 @@
                         
                         <div class="list-group">
                             <a href="#" class="list-group-item active">
-                                <span class="badge">20</span>
-                                Geral
+                                <span class="badge">{{ $notes_total }}</span>
+                                Todos
                             </a>
-                            <a href="#" class="list-group-item">
-                                <span class="badge">10</span>
-                                Social
-                            </a>
-                            <a href="#" class="list-group-item">
-                                <span class="badge">8</span>
-                                Útil
-                            </a>
-                            <a href="#" class="list-group-item">
-                                <span class="badge">2</span>
-                                Fitness
-                            </a>
+                            @foreach($categories as $category)
+                                <a href="#" class="list-group-item">
+                                    <span class="badge">{{ $notes_categories_total or '2' }}</span>
+                                    {{ $category->name }}
+                                </a>
+                            @endforeach
                         </div>
 
                     </div>
