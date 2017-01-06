894e8485389f326aaca8f70cffcc49d10bd260aa
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-06 02:07:56 -0200

    Getting data from DB to display in view

--- app/Http/Controllers/Webapp/NoteController.php
+++ app/Http/Controllers/Webapp/NoteController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Webapp;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use App\Model\Webapp\Note;
+use App\Model\Webapp\Category;
 
 class NoteController extends Controller
 {
@@ -22,8 +25,18 @@ class NoteController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Note $note, Category $category)
     {
-        return view('webapp.home');
+        // Current User
+        $user = auth()->user()->id;
+
+        return view('webapp.home', [
+            'notes' => $note->where('user_id', $user)->get(),
+            'categories' => $category->where('user_id', $user)->get(),
+            'notes_total' => $note->where('user_id', $user)->get()->count(),
+            //'notes_categories_total' => $note->where('category_id', )->get()->count()
+
+        ]);
     }
+
 }
