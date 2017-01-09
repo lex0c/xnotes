db07e2973db0912a2f9f3e0fcf8aa135dd6b16e0
Léo Castro <leonardo_carvalho@outlook.com>
2017-01-09 02:00:25 -0200

    Added toastr dependencies

---
+++ public/js/custom.js
@@ -0,0 +1,17 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "5000",
+    "hideDuration": "3000",
+    "timeOut": "5000",
+    "extendedTimeOut": "2000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
